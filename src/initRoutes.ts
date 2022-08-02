3e05fc07bed49be8e72b7c3d52e31b8100abb588
Brandon Fernandez Mejia <brandonprogamer02@gmail.com>
2022-08-01 23:58:51 -0400

    deleted the initRoutes file

--- src/initRoutes.ts
+++
@@ -1,6 +0,0 @@
-import { Express, Router } from "express";
-import authRoutes from "./controllers/auth/routes";
-
-export default function initRoutes(app: Express) {
-  app.use(authRoutes(Router()));
-}
